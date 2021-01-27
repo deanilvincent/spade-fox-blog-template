856504b3fb166bbf14a44f1a18c63122855ae301
Mark Vincent <markdeanilvicente@gmail.com>
2021-01-27 12:46:31 +0800

    initial homepage1

---
+++ src/app/components/homepage/tiles/tiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tiles',
+  templateUrl: './tiles.component.html',
+  styleUrls: ['./tiles.component.css']
+})
+export class TilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
